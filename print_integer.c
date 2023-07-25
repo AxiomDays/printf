0b1f1749de9f1559b8869a6113d82c2c56cf3217
Axiom Days <tobeoke@gmail.com>
2023-07-26 00:21:35 +0100

    ivegivenup

--- print_integer.c
+++ print_integer.c
@@ -4,21 +4,25 @@
 
 int print_integer(int num) {
 	int len = 0;
-	int check = 1;
+	int buff = 1;
 	
     	if (num < 0) {
         	len += _putchar('-');
         	num = -num;
     	}
-
-	for (;num/check > 9;)
-		check *= 10;
-
-	for (;check != 0;)
+	else
 	{
-		len += _putchar('0' + num/check);
-		num %= check;
-		check/=10;
+		num = num;
+	}
+
+	for (;num/buff > 9;)
+		buff *= 10;
+
+	for (;buff != 0;)
+	{
+		len += _putchar('0' + num/buff);
+		num %= buff;
+		buff/=10;
 	}
 
 	return (len);
